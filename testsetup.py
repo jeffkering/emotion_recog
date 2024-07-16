76a6cd23e7d550dee814409d531ef88c2801979c
Jeffrey <jeffkering@gmail.com>
2024-07-16 23:05:23 +0100

    Initial Project Setup

---
+++ testsetup.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import cv2
+import tensorflow as tf 
+import matplotlib
+
+print("NumPy version:", np.__version__)
+print("OpenCV version:", cv2.__version__)
+print("TensorFlow version:", tf.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+
+print("Setup complete!")
