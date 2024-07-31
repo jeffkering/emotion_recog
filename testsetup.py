0e0a68a4463e062e3253eb64cc1f0c7f7beacdb4
Jeffrey <jeffkering@gmail.com>
2024-07-31 22:54:44 +0100

    Did some light codding work

--- testsetup.py
+++ testsetup.py
@@ -1,11 +1,45 @@
 import numpy as np 
 import cv2
 import tensorflow as tf 
-import matplotlib
+import matplotlib.pyplot as plt
+import os
+current_dir = os.getcwd()
+print(current_dir)
 
-print("NumPy version:", np.__version__)
-print("OpenCV version:", cv2.__version__)
-print("TensorFlow version:", tf.__version__)
-print("Matplotlib version:", matplotlib.__version__)
 
-print("Setup complete!")
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.models import Model 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+
+print("Tensorflow version: ", tf.__version__)
+
+# working with base model
+base_model = MobileNet( input_shape=(224,224,3), include_top=False)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = Flatten()(base_model.output)
+x = Dense(units=7 , activation='softmax' )(x)
+
+#creating model
+model = Model(base_model.input, x)
+
+model.compile(optimizer='adam', loss= categorical_crossentropy , metrics=['accuracy']  )
+
+train_datagen = ImageDataGenerator(
+    zoom_range = 0.2,
+    shear_range = 0.2,
+    horizontal_flip = True,
+    rescale = 1./255
+)
+
+train_data = train_datagen.flow_from_directory(directory = r"C:\Git\emotion_recog\emotion_recog\emotion_recog_env\trainingdata\train",
+                                               target_size = (224,224),
+                                               batch_size = 32,
+                                )
+
+train_data.class_indices
